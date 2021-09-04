b42f70273bacd28e8c43a4364ab222b9eb1d6cdf
Milica <djoric.milica91@yahoo.com>
2021-09-04 12:13:25 -0400

    Initial commit

---
+++ src/test/java/b22/spartan/admin/SpartanAdminGetTest.java
@@ -0,0 +1,54 @@
+package b22.spartan.admin;
+
+import io.restassured.http.ContentType;
+import net.serenitybdd.junit5.SerenityTest;
+import net.serenitybdd.rest.SerenityRest;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.baseURI;
+
+import static net.serenitybdd.rest.SerenityRest.given;
+
+@SerenityTest
+public class SpartanAdminGetTest {
+    @BeforeAll
+    public static void init(){
+        //save baseurl inside this variable so that we dont need to type each http method.
+        baseURI = "http://44.195.19.167:7000";
+
+    }
+
+    @Test
+    public void getAllSpartan(){
+
+        given()
+                .accept(ContentType.JSON)
+                .and()
+                .auth().basic("admin","admin")
+                .when()
+                .get("/api/spartans")
+                .then()
+                .statusCode(200)
+                .and()
+                .contentType(ContentType.JSON);
+
+    }
+    @Test
+    public void getOneSpartan(){
+
+       given()
+                .accept(ContentType.JSON)
+                .and()
+                .auth().basic("admin","admin")
+               .pathParam("id", "15")
+                .when()
+                .get("/api/spartans")
+                .then()
+                .statusCode(200)
+                .and()
+                .contentType(ContentType.JSON);
+
+    }
+
+}
